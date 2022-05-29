bd38cfd0d0499a07c8a2764011bf0799b4140769
AlexErmakov77git config user.name git config --global user.name AlexErmakov77 <aaermakov77@gmail.com>
2022-05-29 14:03:58 +0300

    hw02_unpack_string completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,69 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
+func Unpack(s string) (string, error) {
 	// Place your code here.
-	return "", nil
+
+	//s := "a4bc2d5ehg0hhh0"
+	//s := "арпа3пр6"
+	sr := []rune(s)
+	var s2 string
+	var s3 string
+	var sb strings.Builder
+	var t bool = true
+
+	for i := 0; i < len(sr); i++ {
+		if unicode.IsDigit(sr[0]) {
+			t = false
+
+		}
+		if i < len(sr)-1 && unicode.IsDigit(sr[i]) && unicode.IsDigit(sr[i+1]) {
+			t = false
+
+		}
+
+	}
+
+	switch t {
+
+	case false:
+		//println("error")
+		return "", ErrInvalidString
+
+	case true:
+		{
+			for i := 0; i < len(sr); {
+
+				if unicode.IsDigit(sr[i+1]) {
+					//s2 = string(sr[i+1])
+					res, err := strconv.Atoi(string(sr[i+1])) //(s2)
+					if err != nil {
+						panic(err)
+					}
+					//println(sr[i], "*", string(sr[i]), res)
+
+					s3 = (strings.Repeat(string(sr[i]), res))
+					sb.WriteString(s3)
+					i = i + 2
+
+				} else {
+					sb.WriteString(string(sr[i]))
+					i = i + 1
+				}
+			}
+
+			//println(sb.String())
+			s2 = sb.String()
+		}
+
+	}
+	return s2, nil
+	//return "", nil
 }
