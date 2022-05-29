802a13ab762ea68034000be0608bec464b435bcd
AlexErmakov77git config user.name git config --global user.name AlexErmakov77 <aaermakov77@gmail.com>
2022-05-29 19:42:19 +0300

    hw02

--- hw02_unpack_string/go.mod
+++ hw02_unpack_string/go.mod
@@ -1,5 +1,6 @@
 module github.com/fixme_my_friend/hw02_unpack_string
 
+
 go 1.16
 
 require (
--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -5,27 +5,30 @@ import (
 	"strconv"
 	"strings"
 	"unicode"
+	"unicode/utf8"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(s string) (string, error) {
-	// Place your code here.
 
-	//s := "a4bc2d5ehg0hhh0"
-	//s := "арпа3пр6"
 	sr := []rune(s)
 	var s2 string
 	var s3 string
 	var sb strings.Builder
 	var t bool = true
+	lenS := utf8.RuneCountInString(string(sr))
 
-	for i := 0; i < len(sr); i++ {
-		if unicode.IsDigit(sr[0]) {
+	if s == "" {
+		return "", nil
+	}
+
+	for i := 0; i < lenS; i++ {
+		if i == 0 && unicode.IsDigit(sr[i]) {
 			t = false
 
 		}
-		if i < len(sr)-1 && unicode.IsDigit(sr[i]) && unicode.IsDigit(sr[i+1]) {
+		if i < lenS-2 && unicode.IsDigit(sr[i]) && unicode.IsDigit(sr[i+1]) {
 			t = false
 
 		}
@@ -35,36 +38,34 @@ func Unpack(s string) (string, error) {
 	switch t {
 
 	case false:
-		//println("error")
 		return "", ErrInvalidString
 
 	case true:
-		{
-			for i := 0; i < len(sr); {
+		for i := 1; i < lenS-1; {
 
-				if unicode.IsDigit(sr[i+1]) {
-					//s2 = string(sr[i+1])
-					res, err := strconv.Atoi(string(sr[i+1])) //(s2)
-					if err != nil {
-						panic(err)
-					}
-					//println(sr[i], "*", string(sr[i]), res)
+			if unicode.IsDigit(sr[i]) {
 
-					s3 = (strings.Repeat(string(sr[i]), res))
-					sb.WriteString(s3)
-					i = i + 2
-
-				} else {
-					sb.WriteString(string(sr[i]))
-					i = i + 1
+				res, err := strconv.Atoi(string(sr[i])) //, err
+				if err != nil {
+					panic(err)
 				}
-			}
 
-			//println(sb.String())
-			s2 = sb.String()
+				s3 = (strings.Repeat(string(sr[i-1]), res))
+				sb.WriteString(s3)
+				i += 2
+
+			} else {
+				sb.WriteString(string(sr[i-1]))
+				i++
+			}
 		}
 
+		if !unicode.IsDigit(sr[lenS-1]) {
+			sb.WriteString(string(sr[lenS-1]))
+		}
+		s2 = sb.String()
+
 	}
+
 	return s2, nil
-	//return "", nil
 }
