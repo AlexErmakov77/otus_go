523f7c1750088266e609051c61f649c6b82ecc2b
AlexErmakov77git config user.name git config --global user.name AlexErmakov77 <aaermakov77@gmail.com>
2022-05-31 10:41:38 +0300

    new unpak.go

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -12,60 +12,61 @@ var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(s string) (string, error) {
 
-	sr := []rune(s)
 	var s2 string
 	var s3 string
 	var sb strings.Builder
 	var t bool = true
-	lenS := utf8.RuneCountInString(string(sr))
 
 	if s == "" {
 		return "", nil
-	}
+	} else {
+		sr := []rune(s)
+		lenS := utf8.RuneCountInString(string(sr))
 
-	for i := 0; i < lenS; i++ {
-		if i == 0 && unicode.IsDigit(sr[i]) {
-			t = false
+		for i := 0; i < lenS; i++ {
+			if i == 0 && unicode.IsDigit(sr[i]) {
+				t = false
 
-		}
-		if i < lenS-2 && unicode.IsDigit(sr[i]) && unicode.IsDigit(sr[i+1]) {
-			t = false
-
-		}
-
-	}
-
-	switch t {
-
-	case false:
-		return "", ErrInvalidString
-
-	case true:
-		for i := 1; i < lenS-1; {
-
-			if unicode.IsDigit(sr[i]) {
-
-				res, err := strconv.Atoi(string(sr[i])) //, err
-				if err != nil {
-					panic(err)
-				}
-
-				s3 = (strings.Repeat(string(sr[i-1]), res))
-				sb.WriteString(s3)
-				i += 2
-
-			} else {
-				sb.WriteString(string(sr[i-1]))
-				i++
 			}
+			if i < lenS-2 && unicode.IsDigit(sr[i]) && unicode.IsDigit(sr[i+1]) {
+				t = false
+
+			}
+
 		}
 
-		if !unicode.IsDigit(sr[lenS-1]) {
-			sb.WriteString(string(sr[lenS-1]))
-		}
-		s2 = sb.String()
+		switch t {
 
+		case false:
+			return "", ErrInvalidString
+
+		case true:
+			for i := 1; i < lenS; {
+
+				if unicode.IsDigit(sr[i]) {
+
+					res, err := strconv.Atoi(string(sr[i])) //, err
+					if err != nil {
+						panic(err)
+					}
+
+					s3 = (strings.Repeat(string(sr[i-1]), res))
+					sb.WriteString(s3)
+					i += 2
+
+				} else {
+					sb.WriteString(string(sr[i-1]))
+					i++
+				}
+			}
+
+			if !unicode.IsDigit(sr[lenS]) {
+				sb.WriteString(string(sr[lenS]))
+			}
+			s2 = sb.String()
+
+		}
+
+		return s2, nil
 	}
-
-	return s2, nil
 }
