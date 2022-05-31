6683e40179c0da5ab92e172de7a648a378db29f4
AlexErmakov77git config user.name git config --global user.name AlexErmakov77 <aaermakov77@gmail.com>
2022-05-31 23:30:10 +0300

    hw02 completed linter

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -11,12 +11,11 @@ import (
 var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(s string) (string, error) {
-
 	var s2 string
 	var s3 string
 	var sb strings.Builder
-	var t bool = true
-
+	var t bool
+	t = true
 	if s == "" {
 		return "", nil
 	} else {
@@ -26,31 +25,24 @@ func Unpack(s string) (string, error) {
 		for i := 0; i < lenS; i++ {
 			if i == 0 && unicode.IsDigit(sr[i]) {
 				t = false
-
 			}
 			if i < lenS-1 && unicode.IsDigit(sr[i]) && unicode.IsDigit(sr[i+1]) {
 				t = false
-
 			}
-
 		}
 
 		switch t {
-
 		case false:
 			return "", ErrInvalidString
 
 		case true:
 			for i := 0; i < lenS; i++ {
-
 				if !unicode.IsDigit(sr[i]) && !unicode.IsDigit(sr[i+1]) {
 					sb.WriteString(string(sr[i]))
-
 				}
 
 				if !unicode.IsDigit(sr[i]) && unicode.IsDigit(sr[i+1]) {
-
-					counti, err := strconv.Atoi(string(sr[i+1])) //, err
+					counti, err := strconv.Atoi(string(sr[i+1]))
 					if err != nil {
 						panic(err)
 					}
@@ -60,7 +52,6 @@ func Unpack(s string) (string, error) {
 
 				if unicode.IsDigit(sr[i]) && !unicode.IsDigit(sr[i+1]) {
 					continue
-
 				}
 			}
 
@@ -69,9 +60,7 @@ func Unpack(s string) (string, error) {
 			}
 
 			s2 = sb.String()
-
 		}
 		return s2, nil
 	}
-
 }
