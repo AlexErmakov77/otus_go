64f0b8d629ed214657a9568c32306ea8a434deb0
AlexErmakov77git config user.name git config --global user.name AlexErmakov77 <aaermakov77@gmail.com>
2022-06-04 14:00:15 +0300

    hw03 completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,52 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"regexp"
+	"sort"
+)
+
+type dictWord struct {
+	word  string
+	count int
+}
+
+var reg = regexp.MustCompile(`[^\r\n\t\f\v\s]+`)
+
+func Top10(str string) []string {
+	if len(str) == 0 {
+		return nil
+	}
+	words := make(map[string]int)
+
+	res := reg.FindAllString(str, -1)
+	for i := range res {
+		word := string(res[i])
+		words[word]++
+	}
+
+	top := make([]dictWord, 0, len(words))
+	for w, c := range words {
+		top = append(top, dictWord{w, c})
+	}
+
+	sort.Slice(top, func(i int, j int) bool {
+		if top[i].count == top[j].count {
+			return top[i].word < top[j].word
+		}
+		return top[i].count > top[j].count
+	})
+
+	lenList := len(top)
+	if lenList > 10 {
+		lenList = 10
+	}
+
+	topSlice := make([]string, 0, lenList)
+	for i, v := range top {
+		if i < lenList {
+			topSlice = append(topSlice, v.word)
+		}
+	}
+	return topSlice
+
 }
