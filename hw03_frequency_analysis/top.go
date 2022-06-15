5304ebc8106cdbe2e4c6ac94b614a3192c981754
Александр <99829795+AlexErmakov77@users.noreply.github.com>
2022-06-15 19:54:16 +0300

    Revert "Hw03 frequency analysis"

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,47 +1,6 @@
 package hw03frequencyanalysis
 
-import (
-	"regexp"
-	"sort"
-)
-
-type dictWord struct {
-	word  string
-	count int
-}
-
-var reg = regexp.MustCompile(`[^\r\n\t\f\v\s]+`)
-
-func Top10(str string) []string {
-	if len(str) == 0 {
-		return nil
-	}
-	words := make(map[string]int)
-
-	res := reg.FindAllString(str, -1)
-	for i := range res {
-		word := res[i]
-		words[word]++
-	}
-	top := make([]dictWord, 0, len(words))
-	for w, c := range words {
-		top = append(top, dictWord{w, c})
-	}
-	sort.Slice(top, func(i int, j int) bool {
-		if top[i].count == top[j].count {
-			return top[i].word < top[j].word
-		}
-		return top[i].count > top[j].count
-	})
-	lenList := len(top)
-	if lenList > 10 {
-		lenList = 10
-	}
-	topSlice := make([]string, 0, lenList)
-	for i, v := range top {
-		if i < lenList {
-			topSlice = append(topSlice, v.word)
-		}
-	}
-	return topSlice
+func Top10(_ string) []string {
+	// Place your code here.
+	return nil
 }
