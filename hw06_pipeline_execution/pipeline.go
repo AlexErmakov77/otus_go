4984a404b0a960990a94078912b2e1456c73ab22
AlexErmakov77git config user.name git config --global user.name AlexErmakov77 <aaermakov77@gmail.com>
2022-07-19 22:25:34 +0300

    hw06_pipeline_execution

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,14 +1,51 @@
 package hw06pipelineexecution
 
 type (
-	In  = <-chan interface{}
+	I   = interface{}
+	In  = <-chan I
 	Out = In
-	Bi  = chan interface{}
+	Bi  = chan I
 )
 
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	pipelineIn := make(In)
+	pipelineOut := make(Out)
+
+	if len(stages) <= 0 {
+		emptyCh := make(chan I)
+		close(emptyCh)
+		return emptyCh
+
+	}
+
+	for i := 0; i < len(stages); i++ {
+		if i == 0 {
+			pipelineOut = stages[i](in)
+		} else {
+			pipelineOut = stages[i](pipelineIn)
+		}
+		pipelineIn = pipelineOut
+	}
+	proxyOutCh := make(Bi)
+
+	go proxyOutListener(pipelineOut, proxyOutCh, done)
+
+	return proxyOutCh
+}
+
+func proxyOutListener(pipelineOut Out, proxyOutCh Bi, done In) {
+	defer close(proxyOutCh)
+	for {
+		select {
+		case value, ok := <-pipelineOut:
+			if !ok {
+				return
+			}
+			proxyOutCh <- value
+		case <-done:
+			return
+		}
+	}
 }
